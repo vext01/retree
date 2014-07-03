1ac8f7deea81835662918ee14670d0568a141901
Edd Barrett <vext01@gmail.com>
2014-07-03 19:54:09 +0100

    Regexs

--- retree.py
+++ retree.py
@@ -1,35 +1,49 @@
 #!/usr/bin/env python3
-import os, sys
+import os, sys, re
 
 class ReTreeError(Exception): pass
 
-def do_rename(path):
+def do_rename(path, re_pairs):
     # XXX just lowercases for now
-    new_name = path.lower()
+    new_name = path
+    for (re, repl) in re_pairs:
+        new_name = re.sub(repl, new_name)
 
-    print("%s -> %s" % (path, new_name))
-    os.rename(path, new_name)
+    if new_name != path:
+        print("%s -> %s" % (path, new_name))
+        os.rename(path, new_name)
 
-def recurse(path):
+def recurse(path, re_pairs):
     for i in os.listdir(path):
-        do_rename(os.path.join(path, i))
+        do_rename(os.path.join(path, i), re_pairs)
 
     # reread, since the filenames could have changed
     for i in os.listdir(path):
         full_path = os.path.join(path, i)
         if os.path.isdir(full_path):
-            recurse(full_path)
+            recurse(full_path, re_pairs)
+
+def usage():
+        print("usage: retree <start_path> <pat_!> <repl_1> ... <pat_n> <repl_n>")
+        sys.exit(1)
 
 def entry_point():
     # XXX read regex from commandline
 
     try:
-        start_path = os.path.abspath(sys.argv[1])
+        start_path = sys.argv[1]
     except IndexError:
-        print("usage: retree <start_path>")
-        sys.exit(1)
+        usage()
 
-    recurse(start_path)
+    regexs = sys.argv[2:]
+    if not regexs or len(regexs) % 2 != 0:
+        usage()
+
+    re_pairs = [ (re.compile(regexs[i*2]), regexs[i*2+1]) for
+            i in range(int(len(regexs) / 2)) ]
+
+    os.chdir(start_path)
+    recurse(".", re_pairs)
 
 if __name__ == "__main__":
     entry_point()
